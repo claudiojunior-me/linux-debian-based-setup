300ecb768164276d13f097710633b2c7f003e425
Claudio Junior <claudio.1091@gmail.com>
2019-10-25 21:51:49 -0300

    Added nvm install

---
+++ scripts/git.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo ""
+echo "#############################"
+echo "# Configurig Git"
+echo "#############################"
+echo ""
+
+git config --global user.email "claudio.1091@gmail.com"
+git config --global user.name "Claudio Junior"
